ee1a15ee65ff96df97065d365b65c2b5eb7bae7d
Rafaela Cristina <rafamendes@unifei.edu.br>
2025-09-29 23:56:42 -0300

    atualiza botao de sair da tela de creditos

---
+++ Assets/Scripts/Creditos/LoadGameOver.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Creditos
+{
+    public class LoadGameOver : MonoBehaviour
+    {
+        public void MainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+    }
+
+}
